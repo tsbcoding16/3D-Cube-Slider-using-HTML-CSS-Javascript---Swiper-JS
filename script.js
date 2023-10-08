ee606ac576c6072f0f7a1c02273700907f9ee60e
tsbcoding16 <144004910+tsbcoding16@users.noreply.github.com>
2023-10-08 17:28:52 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".mySwiper", {
+  effect: "cube",
+  grabCursor: true,
+  loop: true,
+  cubeEffect: {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+    delay: 3000,
+  },
+  speed: 1000,
+  mousewheel: {
+    invert: true,
+  },
+});
